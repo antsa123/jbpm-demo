ae134ef5dfc258e2146d99cfcbd236b3251c2af8
Olli Saarinen <Olli Saarinen>
2017-03-09 16:13:08 +0200

    apiKey lisätty ja parit heightit korjattu indexistä

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-﻿$(document).ready(function() {
+const apiKey = "tu705911788977665986179742030436";
+
+$(document).ready(function() {
 	// tähän prosessin aloitus POST
 	
 	$("#nappi").click(function(){
