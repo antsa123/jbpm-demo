70ad8a6b9460ff31927c67a497cd023a71b993bc
Antti Parkkonen <Antti Parkkonen>
2017-03-10 14:18:33 +0200

    Matkavastaus funktio, joka parsii tekstijsonin tiedot oikeille paikoille.

--- jbpmloma/static/js/script.js
+++ jbpmloma/static/js/script.js
@@ -1,7 +1,29 @@
 const apiKey = "tu705911788977665986179742030436";
 
 $(document).ready(function() {
+	
 	// tähän prosessin aloitus POST
+	$.ajax({
+		type: "POST"
+		url: "", //mikä approute
+		
+	});
+	
+	function matkavastaus(data) {
+		obj = JSON.parse(data)
+		var start = obj.start;
+		var stop = obj.stop;
+		var aika = obj.aika;
+		var hinta = obj.hinta;
+		$("#maaranpaa").text(start + " - " + stop)
+		$("hintajalahtoaika").html("Hinta: " + hinta + "<br>Lähtö huomenna kello " + aika)
+		$("#sailio2").fadeOut();
+		$("#sailio3").fadeIn();
+    }
+	
+	function ostovastaus() {
+		
+	}
 	
 	$("#nappi").click(function(){
 		$("#sailio1").fadeOut();
@@ -11,19 +33,19 @@ $(document).ready(function() {
 		$.ajax({
 			type: "GET",
 			url: "/skyscanner",
-			success: function () {
-
-				$("#sailio2").fadeOut();
-				$("#sailio3").fadeIn();
-            }
-
+			success: matkavastaus
 		});
 	});
 	
 	$("#ostonappi").click(function(){
 		$("#sailio3").fadeOut();
 		$("#sailio2").fadeIn();
+		
+		$.ajax({
+			type: "POST",
+			url: "", //mikä approute
+			success: ostovastaus
+		})
 		// Tähän prosesin onnistunut lopetus POST
-
 	});
 });
